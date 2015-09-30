e80aef50d542cc23894281ac248ea71aeb855ddd
Mofe Lee <mofe@me.com>
2015-09-30 16:47:02 +0800

    refactor function name

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+'use strict';
+
+// var EventEmitter = require('events').EventEmitter;
 var SocketIO = require('socket.io');
 var app;
 
-module.exports = function SSocket(){
-  var services = {
-    listen: listen
+exports = module.exports = createApplication;
+
+function createApplication(){
+  var app = {
+    listen: listen,
+    use: use
   };
 
-  return services;
-};
+  return app;
+}
 
 function listen(port){
   app = SocketIO(port);
+  app.on('connection', function(socket){
+    socket.emit('ping');
+  });
   return app;
+}
+
+function use(){
+
 }
